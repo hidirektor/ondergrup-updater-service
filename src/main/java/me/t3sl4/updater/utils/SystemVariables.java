df1ae41a309b9730820209a702118daee146ae1c
Halil İbrahim Direktör <hidirektor@gmail.com>
2025-01-19 02:20:12 +0300

    Hydraulic Pref Update

--- src/main/java/me/t3sl4/updater/utils/SystemVariables.java
+++ src/main/java/me/t3sl4/updater/utils/SystemVariables.java
@@ -1,14 +1,14 @@
 package me.t3sl4.updater.utils;
 
 public class SystemVariables {
-    public static final String CURRENT_VERSION = "v2.0.0";
+    public static final String CURRENT_VERSION = "v2.0.1";
 
     public static final String REPO_OWNER = "hidirektor";
     public static final String HYDRAULIC_REPO_NAME = "hydraulic-tool-desktop";
 
     public static final String PREF_NODE_NAME = "ondergrup";
     public static final String PREF_UPDATER_KEY = "updater_version";
-    public static final String PREF_HYDRAULIC_KEY = "hydraulic_version";
+    public static final String PREF_HYDRAULIC_KEY = "hydraulic_tool";
     public static final String PREF_DEFAULT_DISPLAY = "default_display";
 
     public static String mainPath;
